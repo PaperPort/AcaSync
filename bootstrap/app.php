bbe9c3117f824477173e0604f03e311b49dc4b39
Shella Febriana <shellafebriana14@gmail.com>
2024-06-12 16:13:38 +0700

    style frontend: up livewire

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // solving THAT 419 BASTARD
+        $middleware->validateCsrfTokens(except: [
+            'livewire/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
