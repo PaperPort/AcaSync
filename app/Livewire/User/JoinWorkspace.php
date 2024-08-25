0b7a7a97e506b096e75b5627cd3a01d8ff4faa59
Shella Febriana <shellafebriana14@gmail.com>
2024-08-26 04:45:27 +0700

    build backend : join workspace refactor : services and session config

--- app/Livewire/User/JoinWorkspace.php
+++ app/Livewire/User/JoinWorkspace.php
@@ -2,12 +2,100 @@
 
 namespace App\Livewire\User;
 
+use App\Models\DetailUserWorkspace;
+use App\Models\User;
+use App\Models\UserWorkspace;
+use App\Models\Workspace;
+use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+
 
 #[Title('Gabung Workspace')]
 class JoinWorkspace extends Component
 {
+    public $search, $workspace;
+    public $workspace_id, $kode_akses;
+    public $showModal = false;
+    public $user_id, $user;
+
+
+    public function mount()  {
+        $this->user_id = Session::get('user_id');
+        $this->user    = User::findOrFail($this->user_id);
+    }
+    // open close modal live
+    public function toggleModal($id) {
+        $this->showModal = !$this->showModal;
+        $this->reset(['workspace_id', 'kode_akses']);
+        $this->workspace_id = $id;
+    }
+
+    // update search workspace
+    public function updatedSearch() {
+        if ($this->search == '') {
+            $this->workspace = '';
+        }else{
+            $this->searchWorkspace();
+        }
+    }
+
+    // search workspace
+    public function searchWorkspace() {
+        $search = $this->search;
+        if ($search != '') {
+            $joinedWorkspaceIds  = UserWorkspace::where(
+                'user_id', $this->user_id)->pluck('workspace_id')->all();
+            $this->workspace = Workspace::with('user')
+                    ->whereHas('user', function ($q) use ($search) {
+                        $q->where('name', 'like', '%' . $search . '%')
+                            ->orWhere('user_id', 'like', '%' . $search . '%');
+                    })
+                    ->whereNotIn('id', $joinedWorkspaceIds)
+                    ->get()->toArray();
+        }
+    }
+
+    // join workspace
+    public function joinWorkspace() {
+        $this->validate([
+            'workspace_id'  => 'required',
+            'kode_akses'    => 'required|min:16'
+        ]);
+        
+        $isUserinWorkspace = UserWorkspace::where([
+            ['workspace_id', '=', $this->workspace_id],
+            ['user_id', '=', $this->user_id]
+        ])->exists();
+        
+        $workspace = Workspace::with('user')->where([
+            ['id', '=', $this->workspace_id],
+            ['kode_akses', '=',$this->kode_akses]
+        ])->first();
+
+        if (is_null($workspace)) {
+            flash('Pastikan sudah memasukkan kode akses dengan benar.', 'error');
+        }else if(!$isUserinWorkspace) {
+            $shortID    = substr($workspace->user->user_id, 0,8);
+            $user       = User::findorFail($this->user_id);
+            $userShort  = substr($user->user_id,0,8);
+            $detailUser = DetailUserWorkspace::firstOrCreate(
+                ['nomor_induk' => '', 'tahun_ajaran' => now()->year, 'is_active' => '1', 'created_at'    => now(),]
+            );
+
+            UserWorkspace::firstOrCreate(
+                ['workspace_id' => $this->workspace_id, 'user_id' => $this->user_id],
+                ['detailUser_id' => $detailUser->id, 'rw_id' => null, 'akun_terkait' => null]
+            );
+            return redirect()->route('dashboard-user', ['workspace_id' => $shortID, 'user_id' => $userShort]);
+        }else{
+            flash('Anda sudah menjadi anggota workspace ini.', 'error');
+        }
+
+        $this->showModal = !$this->showModal;
+        $this->reset(['workspace_id', 'kode_akses']);
+    }
+
     public function render()
     {
         return view('livewire.user.join-workspace');
--- resources/views/livewire/user/join-workspace.blade.php
+++ resources/views/livewire/user/join-workspace.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-header.user-navigation/>
+    <x-header.user-navigation :user="$user"/>
 
     <section class="bg-blue" id="banner">
         <div class="flex flex-col max-w-screen-xl px-4 pt-32 pb-20 mx-auto">
@@ -7,16 +7,17 @@
         </div>
     </section>
 
+    <x-alerts.toast/>
     <section id="search-workspace" class="max-w-screen-xl px-4 mx-auto">
         <div class="flex justify-center w-full p-4 mt-3 border border-gray-300 rounded-lg shadow bg-gray-50 lg:-mt-8 md:flex-none md:justify-normal">
             
-            <form class="flex items-center w-full">   
+            <form class="flex items-center w-full" wire:submit='searchWorkspace'>   
                 <label for="simple-search" class="sr-only">Search</label>
                 <div class="relative w-full">
                     <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
                         <i class="w-4 h-4 text-gray-500 fa-solid fa-people-roof"></i>
                     </div>
-                    <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky focus:border-sky block w-full ps-10 p-2.5  " placeholder="Cari nama workspace yang ingin diikuti..." required />
+                    <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky focus:border-sky block w-full ps-10 p-2.5  " placeholder="Cari workspace yang ingin diikuti..." required wire:model.live='search'/>
                 </div>
                 <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-blue rounded-lg border border-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -30,55 +31,16 @@
     </section>
 
     <section id="card-workspace" class="max-w-screen-xl px-4 mx-auto my-5">
-        <h6 class="mb-5 font-light text-gray-500 font-default">Workspace Terkait :</h6>
-        <div class="w-full p-5 bg-white border border-gray-200 rounded-lg shadow">
-            <div class="flex flex-col">
-                <div class="flex items-center gap-2">
-                    <img class="h-24 mb-3 shadow-lg" src="{{ URL::to('/') }}/img/school-bg.png" alt="Bonnie image"/>
-                    <div class="flex flex-col ml-2">
-                        <h5 class="mb-1 text-xl font-medium text-gray-900">Academic University</h5>
-                        <span class="text-sm text-gray-500">Indonesia</span>
-                    </div>
-                    
-                </div>
-                <div class="flex justify-end pt-2 mt-4 border-t-2 border-gray-200 md:mt-6">
-                    <button data-modal-target="authentication-join-workspace-modal" data-modal-toggle="authentication-join-workspace-modal" class="block text-blue outline outline-blue outline-1 hover:bg-sky hover:text-white hover:outline-none focus:ring-4 focus:outline-none focus:ring-sky font-medium rounded-full text-sm px-5 py-2.5 text-center" type="button">
-                        Gabung
-                    </button>
-                </div>
-            </div>
-        </div>
+        @if (!empty($workspace))
+            <h6 class="mb-5 font-light text-gray-500 font-default">Workspace Terkait :</h6>
+            @foreach ($workspace as $data)
+                <x-card.workspace-card title="{{ $data['user']['name'] }}" content="{{ $data['provinsi'] }}" src="{{ $data['user']['profile'] }}" :modal="true" id="{{ $data['id'] }}" showModal="{{ $showModal }}"></x-card.workspace-card>
+            @endforeach
+        @else
+            <x-card.blank-card>Tidak ada yang dicari.</x-card.blank-card>
+        @endif
+        
     </section>
-
-    <div id="authentication-join-workspace-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative w-full max-w-md max-h-full p-4">
-            <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow">
-                <!-- Modal header -->
-                <div class="flex items-center justify-between p-4 border-b rounded-t md:p-5">
-                    <h3 class="text-xl font-semibold text-gray-900">
-                        Kode Masuk Workspace
-                    </h3>
-                    <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="authentication-join-workspace-modal">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                </div>
-                <!-- Modal body -->
-                <div class="p-4 md:p-5">
-                    <form class="space-y-4" method="post" action="">
-                        <div>
-                            <label for="kode_masuk" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kode Masuk</label>
-                            <input type="text" name="kode_masuk" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue focus:border-blue block w-full p-2.5"required />
-                        </div>
-                        <button type="submit" class="w-full text-white bg-blue hover:bg-sky focus:ring-4 focus:outline-none focus:ring-blue font-medium rounded-lg text-sm px-5 py-2.5 text-center">Gabung ke Workspace</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> 
-
+    
     <x-footer.user-footer></x-footer.user-footer>
 </div>
